7986fcfd3130d3057fa802b2396e36e9ae6518b6
4L3xD <alessandra.olea@gmail.com>
2024-04-01 14:44:52 -0300

    add src dir

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 Executar o projeto
 ```
-poetry run python3 main.py
+poetry run python3 src/main.py
 ```
 
 [Poetry Docs](https://python-poetry.org/docs/basic-usage/)
--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from gemini import Gemini
 def main():
     gemini = Gemini()
     print(gemini.nlp())
+    #output > arquivo.json
+    """payload, output, vulnerability"""
 
 if __name__ == "__main__":
     main()
